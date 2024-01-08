498d08be9a622d85a491bbeeccdc34dbd61a8ef2
Erik-git21 <61649454+Erik-git21@users.noreply.github.com>
2024-01-08 22:25:58 +0100

    Add files via upload

---
+++ src/main/java/com/books/bookStore/service/MyBookListService.java
@@ -0,0 +1,31 @@
+package com.books.bookStore.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.books.bookStore.entity.MyBookList;
+import com.books.bookStore.repository.MyBookRepository;
+
+@Service
+public class MyBookListService {
+
+	
+	@Autowired
+	private MyBookRepository mybook;
+	
+	public void saveMyBook(MyBookList book) {
+		mybook.save(book);
+	}
+	
+	public List<MyBookList> getAllMyBooks() {
+		return mybook.findAll();
+	}
+	
+	public void deleteById(int id) {
+		mybook.deleteById(id);
+	}
+	
+	
+}
